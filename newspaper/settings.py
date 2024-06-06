cc916a2ad0728f399421b4d374db8e0a29584e85
Victorspec <generalspectec@gmail.com>
2024-06-06 03:29:01 +0100

    my first commit change

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -120,21 +120,27 @@ WSGI_APPLICATION = 'newspaper.wsgi.application'
 
 
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'emma',
+        #'USER': 'postgres',
+        #'PASSWORD': '1232478847',
+        #'HOST': '127.0.0.1',
+        #'PORT': '5432',
+    #}
+#}
+    
+    
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'emma',
-        'USER': 'postgres',
-        'PASSWORD': '1232478847',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-    
-    
-    
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:1232478847@localhost:5432/emma',
+        conn_max_age=600
+    )
+}    
 
-DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
 
 
 
